4ee87f06951e84000d9e25dc14b7d6d63f538fb6
DinushaLakshan <dinusha9632@gmail.com>
2020-07-28 21:01:30 +0530

    Initialcommit

---
+++ src/business/BusinessLogic.java
@@ -0,0 +1,175 @@
+package business;
+
+import dao.CustomerDAO;
+import dao.ItemDAO;
+import dao.OrderDAO;
+import dao.OrderDetailDAO;
+import db.DBConnection;
+import entity.Customer;
+import entity.Item;
+import entity.Order;
+import entity.OrderDetail;
+import util.CustomerTM;
+import util.ItemTM;
+import util.OrderDetailTM;
+import util.OrderTM;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusinessLogic {
+
+    public static String getNewCustomerId() {
+        String lastCustomerId = CustomerDAO.getLastCustomerId();
+        if (lastCustomerId == null) {
+            return "C001";
+        } else {
+            int maxId = Integer.parseInt(lastCustomerId.replace("C", ""));
+            maxId = maxId + 1;
+            String id = "";
+            if (maxId < 10) {
+                id = "C00" + maxId;
+            } else if (maxId < 100) {
+                id = "C0" + maxId;
+            } else {
+                id = "C" + maxId;
+            }
+            return id;
+        }
+    }
+
+    public static String getNewItemCode() {
+        String lastItemCode = ItemDAO.getLastItemCode();
+        if (lastItemCode == null) {
+            return "I001";
+        } else {
+            int maxId = Integer.parseInt(lastItemCode.replace("I", ""));
+            maxId = maxId + 1;
+            String id = "";
+            if (maxId < 10) {
+                id = "I00" + maxId;
+            } else if (maxId < 100) {
+                id = "I0" + maxId;
+            } else {
+                id = "I" + maxId;
+            }
+            return id;
+        }
+    }
+
+    public static String getNewOrderId() {
+        String lastOrderId = OrderDAO.getLastOrderId();
+        if (lastOrderId == null) {
+            return "OD001";
+        } else {
+            int maxId = Integer.parseInt(lastOrderId.replace("OD", ""));
+            maxId = maxId + 1;
+            String id = "";
+            if (maxId < 10) {
+                id = "OD00" + maxId;
+            } else if (maxId < 100) {
+                id = "OD0" + maxId;
+            } else {
+                id = "OD" + maxId;
+            }
+            return id;
+        }
+    }
+
+    public static List<CustomerTM> getAllCustomers() {
+        List<Customer> allCustomers = CustomerDAO.findAllCustomers();
+        List<CustomerTM> customers = new ArrayList<>();
+        for (Customer customer : allCustomers) {
+            customers.add(new CustomerTM(customer.getId(), customer.getName(), customer.getAddress()));
+        }
+        return customers;
+    }
+
+    public static boolean saveCustomer(String id, String name, String address) {
+        return CustomerDAO.saveCustomer(new Customer(id, name, address));
+    }
+
+    public static boolean deleteCustomer(String customerId) {
+        return CustomerDAO.deleteCustomer(customerId);
+    }
+
+    public static boolean updateCustomer(String name, String address, String customerId) {
+        return CustomerDAO.updateCustomer(new Customer(customerId, name, address));
+    }
+
+    public static List<ItemTM> getAllItems() {
+        List<Item> allItems = ItemDAO.findAllItems();
+        List<ItemTM> items = new ArrayList<>();
+        for (Item item : allItems) {
+            items.add(new ItemTM(item.getCode(), item.getDescription(), item.getQtyOnHand(),
+                    item.getUnitPrice().doubleValue()));
+        }
+        return items;
+    }
+
+    public static boolean saveItem(String code, String description, int qtyOnHand, double unitPrice) {
+        return ItemDAO.saveItem(new Item(code, description, BigDecimal.valueOf(unitPrice), qtyOnHand));
+    }
+
+    public static boolean deleteItem(String itemCode) {
+        return ItemDAO.deleteItem(itemCode);
+    }
+
+    public static boolean updateItem(String description, int qtyOnHand, double unitPrice, String itemCode) {
+        return ItemDAO.updateItem(new Item(itemCode, description,
+                BigDecimal.valueOf(unitPrice), qtyOnHand));
+    }
+
+    public static boolean placeOrder(OrderTM order, List<OrderDetailTM> orderDetails) {
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            connection.setAutoCommit(false);
+            boolean result = OrderDAO.saveOrder(new Order(order.getOrderId(),
+                    Date.valueOf(order.getOrderDate()),
+                    order.getCustomerId()));
+            if (!result) {
+                connection.rollback();
+                return false;
+            }
+            for (OrderDetailTM orderDetail : orderDetails) {
+                result = OrderDetailDAO.saveOrderDetail(new OrderDetail(
+                        order.getOrderId(), orderDetail.getCode(),
+                        orderDetail.getQty(), BigDecimal.valueOf(orderDetail.getUnitPrice())
+                ));
+                if (!result){
+                    connection.rollback();
+                    return false;
+                }
+                Item item = ItemDAO.findItem(orderDetail.getCode());
+                item.setQtyOnHand(item.getQtyOnHand() - orderDetail.getQty());
+                result = ItemDAO.updateItem(item);
+                if (!result){
+                    connection.rollback();
+                    return false;
+                }
+            }
+            connection.commit();
+            return true;
+        } catch (Throwable throwables) {
+            throwables.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return false;
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+
+}
---
+++ test/dao/CustomerDAOTest.java
@@ -0,0 +1,24 @@
+package dao;
+
+import entity.Customer;
+
+import java.util.List;
+
+public class CustomerDAOTest {
+
+    public static void main(String[] args) {
+        assert CustomerDAO.findAllCustomers().size() == 6;
+//        assert CustomerDAO.deleteCustomer("C011"): "Test Failed";
+//        boolean result = CustomerDAO.deleteCustomer("C006");
+//        List<Customer> customers = CustomerDAO.findAllCustomers();
+//        for (Customer customer : customers) {
+//            System.out.println(customer);
+//        }
+/*        boolean result = CustomerDAO.saveCustomer(new Customer("C006", "Appu", "Panadura"));
+        System.out.println(result);
+        List<Customer> customers = CustomerDAO.findAllCustomers();
+        for (Customer customer : customers) {
+            System.out.println(customer);
+        }*/
+    }
+}
